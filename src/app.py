b9fd034a1abb3a00631fc69b5140567d95d707cf
Bego Gonzalez <bego.gonzalez@outlook.com>
2024-05-16 19:06:25 +0200

    Renamed __init__

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 import os
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
-from flask_swagger import swagger
+#from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
 from api.models import db
 from api.routes import api
