a69559cf1f8bad702d0444a9d58f821ae724cb7d
Bego Gonzalez <bego.gonzalez@outlook.com>
2024-05-17 17:03:37 +0200

    arreglando imports

--- src/app.py
+++ src/app.py
@@ -6,10 +6,10 @@ from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 #from flask_swagger import swagger
 from src.api.utils import APIException, generate_sitemap
-from api.models import db
-from api.routes import api
-from api.admin import setup_admin
-from api.commands import setup_commands
+from src.api.models import db
+from src.api.routes import api
+from src.api.admin import setup_admin
+from src.api.commands import setup_commands
 from flask_jwt_extended import JWTManager
 
 # from models import Person
